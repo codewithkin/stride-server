41aac867b7c2f6e9318d8c9244e620ef49cc99a4
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-07-25 21:44:31 +0200

    [ADD]: Sign in with magic link code

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import { Hono } from "hono";
 import type { AuthType } from "./lib/auth"
 import auth from "./routes/auth";
+import { logger } from "hono/logger";
 
 const app = new Hono<{ Variables: AuthType }>({
   strict: false,
 });
+
+app.use("*", logger());
 
 const routes = [auth] as const;
 
